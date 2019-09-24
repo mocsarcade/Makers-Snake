c0199e3fee2e58b948902107f86da03d804edb25
Sevaughn Orr <sevaughnorr@gmail.com>
2019-09-24 17:27:04 -0400

    snake now uses body instead of position

--- snake.js
+++ snake.js
@@ -6,26 +6,33 @@ export default class Snake
     {
         this.width = 30;
         this.height = 30;
-        this.body = [];
+        this.body = [
+            [150, 50], [120, 50]
+        ];
         this.direction = DIR.RIGHT;
-        this.position = 
-        {
-            x: x,
-            y: y,
-        }
     }// end constructor
 
     update(deltaTime)
     {
         // console.log(this.direction);
-        this.position.x += this.direction[0];
-        this.position.y += this.direction[1];
+        for (let i = 0; i < this.body.length; i++)
+        {
+            this.body[i][0] += this.direction[0];
+            this.body[i][1] += this.direction[1];
+        }
     }// end method
 
     draw(ctx)
     {
         ctx.beginPath();
+        for (let i = 0; i < this.body.length; i++)
+        {
+            ctx.rect(this.body[i][0], this.body[i][1], this.width, this.height);
+        }
         ctx.fillStyle =  "green";
-        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+        // ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+        ctx.fill();
+        ctx.strokeStyle = "white";
+        ctx.stroke()
     }// end method
 }// end class
