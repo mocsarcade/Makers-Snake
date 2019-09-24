82d4a3aace8e8868493e52d7e50f34b157b65691
Sevaughn Orr <sevaughnorr@gmail.com>
2019-09-24 17:03:19 -0400

    added direction.js for movement

---
+++ direction.js
@@ -0,0 +1,9 @@
+let movementUnits = 5;
+export const DIR = {
+    UP: [0, -movementUnits], 
+    DOWN: [0, movementUnits], 
+    LEFT: [-movementUnits, 0], 
+    RIGHT: [movementUnits, 0]
+};
+
+export default class Direction{}
