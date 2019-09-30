bb18ece5d04d4a19058cd0066e90d07fa15ded8c
Sevaughn Orr <sevaughnorr@gmail.com>
2019-09-29 21:01:00 -0400

    refactored objects into snakeGame

--- main.js
+++ main.js
@@ -1,34 +1,28 @@
-import Snake from "./snake.js";
-import InputHandler from "./inputHandler.js";
+import SnakeGame from "./snakeGame.js";
 // initialize vars
 let cvs = document.getElementById("canvas");
 // this is the context, it's an attribute of the canvas
 // whose methods we can use to draw graphics
 let ctx = cvs.getContext("2d");
 let lastTime = 0;
-let player = new Snake(40, 40, 2);
-let input = new InputHandler(player);
+let snakeGame = new SnakeGame(cvs.width, cvs.height);
+snakeGame.initialize();
 // the main game loop
 
 function gameLoop(timeStamp)
 { 
-    timeStamp = new Date().getTime();
-    // this is how much time has passed since this
-    // function has been last called, this allows
-    // the game to be independant of framerate
-    var deltaTIme = timeStamp - lastTime; 
-    lastTime = timeStamp;  
-    // clear the screen and draw the background
-    ctx.clearRect(0, 0, cvs.width, cvs.height); 
-    ctx.fillStyle = "black";
-    ctx.fillRect(0, 0, cvs.width, cvs.height);
-    
-    player.update(deltaTIme);
-    player.draw(ctx);
-    
-    //requestAnimationFrame(gameLoop);
+	timeStamp = new Date().getTime();
+	// this is how much time has passed since this
+	// function has been last called, this allows
+	// the game to be independant of framerate
+	var deltaTIme = timeStamp - lastTime; 
+	lastTime = timeStamp;  
+	// clear the screen and draw the background
+	snakeGame.update(deltaTIme);
+	snakeGame.draw(ctx);
+	// requestAnimationFrame(gameLoop);
 }// end method
 // tells the browser to call this function right before every render of the page occurs
-//requestAnimationFrame(gameLoop);
-// let gamespeed = 200
-// setInterval(gameLoop, gamespeed);
+// requestAnimationFrame(gameLoop);
+let gamespeed = 300;
+setInterval(gameLoop, gamespeed);
